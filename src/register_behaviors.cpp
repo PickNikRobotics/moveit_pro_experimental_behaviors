ee804d4cdd763496e29a6f5c3307a95dd754efda
Mario Prats <mario.prats@picknik.ai>
2026-02-10 09:04:10 +0100

    Update src/register_behaviors.cpp
    Co-authored-by: Henry Moore <henry.moore@picknik.ai>

--- src/register_behaviors.cpp
+++ src/register_behaviors.cpp
@@ -1,3 +1,9 @@
+// Copyright 2026 PickNik Inc.
+// All rights reserved.
+//
+// Unauthorized copying of this code base via any medium is strictly prohibited.
+// Proprietary and confidential.
+
 #include <behaviortree_cpp/bt_factory.h>
 #include <moveit_pro_behavior_interface/behavior_context.hpp>
 #include <moveit_pro_behavior_interface/shared_resources_node_loader.hpp>
